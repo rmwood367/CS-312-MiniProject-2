a3266e386dee2d471652cb176cde167aeea2a393
rmwood367 <rmw367@nau.edu>
2024-09-30 13:36:31 -0700

    added correct app.js file

---
+++ app.js
@@ -0,0 +1,35 @@
+// Server Set-up
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+//to render my view files
+app.set('view engine', 'ejs');
+// lets app handle form datea
+app.use(express.urlencoded({ extended: true }));
+//render the home page
+app.get('/', (req, res) => 
+{
+  res.render('index');
+});
+// Route to fetch all of the cocktail information from the cocktailDB API
+app.get('/random-cocktail', async (req, res) => 
+{
+  try {
+    const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const cocktail = response.data.drinks[0];
+    console.log(cocktail);
+    //render the cocktail results to cocktailresults.ejs
+    res.render('cocktailresults', { cocktail });
+    //render error message just in case
+  } catch (error) {
+    res.render('error', { message: "Error fetching cocktail. Try again!" });
+  }
+});
+
+//start the server
+app.listen(3000, () => 
+{
+  //message of where it has been started
+  console.log('website is running on port 3000');
+});
